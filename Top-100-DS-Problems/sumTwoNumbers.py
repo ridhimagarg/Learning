8a0be526ccc07d2d0518d16699862fc694db011c
ridhimagarg <garg.ridhima72@gmail.com>
2023-01-22 12:26:13 +0100

    changes

--- Top-100-DS-Problems/sumTwoNumbers.py
+++ Top-100-DS-Problems/sumTwoNumbers.py
@@ -1,7 +1,7 @@
 # Q: Finding the sum of two elements in array which will result the target sum.
 
 
-## Time Compl -: O(n) and Space Compl -: O(n)
+## Time Compl -: O(n) and Space Compl -: O(n) because we are storing an extra hash table
 ## Explanation: Taking a hash table(dictionary) which will contain numbers which we have traversed till now in array.
 ## Taking the current value from array and subtracting from target sum i.e, y= targetSum - CurrentVal and if y is present in hashtable then will return
 ## otherwise keep on adding current value to hashtable.
@@ -18,7 +18,7 @@ def sumTwoNum(array, targetSum):
 
     return []
 
-## Time Compl -: O(nlogn)
+## Time Compl -: O(nlogn), Sapxe -: O(1) as no extra space
 ## Explanation: Sort the array in increasing order which we assumes to be sorted by best sorting algorithm which takes O(nlogn).
 ## Now using two pointers one on left and one on right, taking sum if sum==tagetsum : return , elif sum < targetsum that means we have to increase the sum so if we 
 ## move right pointer to left will decrease the sum, hence will move left pointer hence it will increase the sum. elif sum> targetsum, then will move right pointer to left side.
