ccf1c14c88732a27f4ad32c18095f98f15bce5a9
ridhimagarg <garg.ridhima72@gmail.com>
2023-04-02 18:12:41 +0200

    valid seq

--- Top-100-DS-Problems/validateSubsequence.py
+++ Top-100-DS-Problems/validateSubsequence.py
@@ -8,11 +8,12 @@
 
 def validateSubSeq(array, sequence):
 
+    ## This implementation I think will fail if empty sequence is a valid seq
     if sequence == [] or len(sequence)> len(array):
         return False
 
     subIndx = 0
-
+ 
     for value in array:
         if value == sequence[subIndx]:
             subIndx += 1
@@ -23,3 +24,24 @@ def validateSubSeq(array, sequence):
 
 
 print(validateSubSeq([10,9,-1,8,6,80,0,1], [10,9,-100,10,10,10,10,10,10,10,78]))
+print(validateSubSeq([-1,6,1,0], [10,9,-1,8,6,80,1,0]))
+print(validateSubSeq([10,9,-1,8,6,80,1,0], [-1,6,1,0]))
+
+
+## Another solution using while loop
+## Time: O(N)| Space: O(1)
+
+def validateSubSeq(array, sequence):
+
+    arrIdx = 0
+    seqIdx = 0
+
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx +=1
+        arrIdx +=1
+
+    return seqIdx == len(sequence)
+
+print(validateSubSeq([-1,6,1,0], [10,9,-1,8,6,80,1,0]))
+print(validateSubSeq([10,9,-1,8,6,80,1,0], [-1,6,1,0]))
