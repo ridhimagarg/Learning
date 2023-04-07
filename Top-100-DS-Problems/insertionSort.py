e4907a51550e467a136da2072b0a693eaf5c6df0
ridhimagarg <garg.ridhima72@gmail.com>
2023-04-07 21:15:42 +0200

    insertion sort

---
+++ Top-100-DS-Problems/insertionSort.py
@@ -0,0 +1,16 @@
+
+
+def insertionSort(array):
+
+    for i, e in enumerate(array):
+        for j in range(i, 0, -1):
+            if array[j] < array[j-1]:
+                swap(j, j-1, array)
+
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+
+
+print(insertionSort([1,8,2,9,5,6,3,1,7]))
